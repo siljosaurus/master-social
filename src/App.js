597510c633c774397994362add1c611d0ddb12db
siljosaurus <silje@ignitas.no>
2021-01-05 20:07:03 +0100

    added countdown bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import './App.css'
+import './components/countdown'
+import Countdown from './components/countdown'
 
 const App = () => {
   return(
     <div className="app">
       <h1> social club for dezzignnn </h1>
-      <p>Velkommen til dette eksperimentelle digitale social-club området hvor mastergrupper kan møtes for å løse challenges sammen. </p>
+      <p>Velkommen til dette eksperimentelle digitale social-club området for masterstudenter på design programmet.</p>
+      <Countdown />
     </div>
   )
 }
---
+++ src/components/countdown.js
@@ -0,0 +1,41 @@
+import dayjs from 'dayjs'
+
+const Countdown = () => {
+    
+    const now = dayjs()
+    const skolestart = dayjs('2020-08-12')
+    const skoledager = skolestart.diff('2022-06-01', 'day')
+    const diff = now.diff('2022-06-01', 'day')
+    const prosent = (1-Math.abs(diff)/Math.abs(skoledager))*100
+        
+    return(
+        <div>
+            <p> Dager til masteren skal leveres: {Math.abs(diff)} </p>
+            <p> {prosent.toFixed(0)}% ferdig med master</p>
+            <div style={{
+                width: "100%",
+                backgroundColor: "rgba(var(--sf-blue), 0.5)",
+                borderRadius: "0.3rem",
+                overflow: 'hidden',
+            }}>
+                <div style={{
+                    width: `${prosent}%`,
+                    height: 50,
+                    backgroundColor: "rgba(var(--sf-blue), 1)",
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                }}>
+                   <div style={{
+                       backgroundColor: 'transparent',
+                       textAlign: 'center',
+                       color: '#231F20'
+                   }}> {Math.abs(prosent.toFixed(0))}% ferdig </div>
+                </div>
+                
+                </div>
+        </div>
+    )
+}
+
+export default Countdown;
