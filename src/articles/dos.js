9fc55b78880a8b5bcbf680f5c140aabadffde39e
siljosaurus <silje@ignitas.no>
2021-08-17 14:51:43 +0200

    new info

--- src/articles/dos.js
+++ src/articles/dos.js
@@ -5,30 +5,38 @@ const Dos = () => {
         <div>
             <div style={{border: 'solid rgba(229, 227, 210)'}}></div>
             <h1>Plan for fadderuka</h1>
-            <h4>Publisert: 01.07.2021</h4>
-            <p>Hei igjen, her kommer en liten oppdatering på hvordan fadderuka begynner å ta form! Admin og programleder for design har publisert det faglige opplegget som vil skje onsdag, torsdag og fredag for de nye masterstudentene. Studiestart og offisiell start på fadderuka er derfor <b>onsdag 18. August. </b> Vi er også en gjeng faddersjefer på tvers av alle masterlinjene som har møttes og kommet frem til to litt større sosiale arrangementer på torsdag og fredag som jeg tror blir veldig hyggelig og gøy! Dette søker vi om pengestøtte fra studieadmin til, så vi avventer fortsatt litt om dette går gjennom, men vi krysser fingrene.</p>
-            <p>Nå som vi har oversikt over de formelle tingene som skjer iløpet av fadderuka, kan vi begynne å planlegge det sosiale opplegget vi har lyst på! Siden studiestart for 4. årsstudentene offisielt ikke begynner før på onsdag, tenker jeg at vi feks kan bruke mandag og tirsdag for å gjøre noe sosialt bare for oss 5. årsstudenter/ kullet vårt. Men, dette tenker jeg vi finner ut av sammen.</p>
-            <p><a href="https://www.mn.uio.no/ifi/studier/master/studiestart/">Her er linken til UiO sin offisielle studiestart side som blir sendt ut til de nye studentene. </a>Foreløpig er den ganske tom, det blir vi som fyller på den etterhvert vi har ting litt mer klart. Enn så lenge bruker vi nettsiden her som en dagsplan for oss faddere.</p>
+            <h4>Publisert: 01.07.2021, sist oppdatert: 17.08.2021</h4>
+            <p>Hei, da er det klart for fadderuke! Under ligger tidsplan for opplegget de neste tre dagene. Vi blir fire faddergrupper, og vi fordeler fadderbarn på onsdag rett etter velkomstmøtet.</p>
+            <p>Faddere møter opp onsdag mellom kl 10:30 og senest kl 11:00 utenfor Logo. Vi kan hente røde tskjorter i 3. etasje.</p>
+            <p><a href="https://www.mn.uio.no/ifi/studier/master/studiestart/">Her er linken til UiO sin offisielle studiestart side som blir sendt ut til de nye studentene. </a></p>
             <br/>
             <div className="table">
                 <div className="table">
                     <h2>Onsdag 18. August</h2>
-                    <p>Nye masterstudenter har velkomstmøte (kl. 09:00-11:00)</p>
+                    <p>kl. 09:15-11:00 ---->  Nye masterstudenter har velkomstmøte.</p>
+                    <p>kl. 11:00-11:30 ---->  Felles lunsj og vi deler inn i faddergrupper.</p>
+                    <p>kl. 11:30-13:00 ---->  "Omvisningschallenge" på ifi & blindern.</p>
+                    <p>kl. 14:15-15:00 ---->  Erfaringsdeling fra 5.klassinger om høstfagene.</p>
+                    <p>kl. 16:00-19:00 ---->  Pause, introverts recharge time.</p>
+                    <p>kl. 19:00-00:00 ---->  De som vil drar til et utested.</p>
                 </div>
                 <div className="table">
                 <h2>Torsdag 19. August</h2>
                     <p>Denne dagen er det frist for emnepåmelding!</p>
-                    <p>Nye masterstudenter har spørretime på Zoom (kl. 14:00-15:00)</p>
-                    <p>FELLES: Det leies et utested for alle linjene (kvelden, kanskje 18/19:00 og utover)</p>
+                    <p>kl. 14:15-15:00 ---->  Spørretime på zoom m/ admin, 5. klassinger får eget breakout rom fra 14:30 og utover for å svare på spørsmål.</p>
+                    <p>kl. 16:00-19:00 ---->  Utflukt til Oslo camping! Instituttet dekker et gratis spill.</p>
+                    <p>kl. 19:00-20:00 ---->  Oslo Streetfood</p>
+                    <p>kl. 20:00-00:00 ---->  De som vil drar til et utested.</p>
                 </div>
                 <div className="table">
                 <h2>Fredag 20. August</h2>
-                    <p>Nye masterstudenter har seminar (kl. 09:00-14:00)</p>
-                    <p>FELLES: Volleyballturnering med alle linjene (15:00 og utover)</p>
+                    <p>kl. 09:15-14:00 ---->  Nye masterstudenter har zoom seminar.</p>
+                    <p>kl. 15:00-19:00 ---->  <a href="https://fb.me/e/4xcaEBmV8"> Grill & chill på Sognsvann med alle ifi masterlinjene!</a></p>
+                    <p>kl. 20:00-00:00 ---->  Mulighet for å dra videre til et utested for de som vil.</p>
                 </div>  
             </div>
             <br/>
-            <p>Bare å sende meg en melding om det er noe du lurer på! </p>
+            <p>Bare å sende meg en melding på fb, mobil (99098900), eller mail (siljemfl@ifi.uio.no) om det er noe du lurer på! </p>
             <p>- Silje Marie Flaaten</p>
         </div>
     )
